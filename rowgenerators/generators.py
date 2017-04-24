10957303366f719269923456d01a70a7c2fad04d
Eric Busboom <eric@clarinova.com>
2017-04-24 15:45:53 -0700

    Fixes for python 2.7

--- rowgenerators/generators.py
+++ rowgenerators/generators.py
@@ -623,7 +623,7 @@ class ExcelSource(SourceFile):
 
 class MetapackSource(SourceFile):
     def __init__(self, spec, dflo, cache, working_dir):
-        super().__init__(spec, dflo, cache)
+        super(MetapackSource, self).__init__(spec, dflo, cache)
 
     def __iter__(self):
         from metatab import open_package
@@ -645,7 +645,7 @@ class ProgramSource(Source):
         from os import environ
         import json
 
-        super().__init__(spec, cache)
+        super(ProgramSource, self).__init__(spec, cache)
 
         assert working_dir
 
@@ -701,7 +701,11 @@ class ProgramSource(Source):
 
         p.stdin.write(json.dumps(self.properties).encode('utf-8'))
 
-        r = csv.reader(TextIOWrapper(p.stdout))
+        try:
+            r = csv.reader(TextIOWrapper(p.stdout))
+        except AttributeError:
+            # For Python 2
+            r = csv.reader(p.stdout)
 
         for row in r:
             yield row
@@ -715,7 +719,7 @@ class NotebookSource(Source):
         from os import environ
         import json
 
-        super().__init__(spec, cache)
+        super(NotebookSource, self).__init__(spec, cache)
 
         self.sys_path = sys_path
 
