80004d81f3ee424359302f64a2b0b3bbe6a39803
Eric Busboom <eric@civicknowledge.com>
2018-04-01 08:54:38 -0700

    Fixed imports

--- rowgenerators/__init__.py
+++ rowgenerators/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from .appurl.url import parse_app_url
+from .appurl.url import parse_app_url, Url
 from .appurl.util import get_cache
 from .core import get_generator
+from .source import  Source
 
 
 # from .appurl import parse_app_url, match_url_classes
