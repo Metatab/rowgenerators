bb149ea074b8d5e06683afe230849645df8cddb0
Eric Busboom <eric@civicknowledge.com>
2018-09-04 17:23:29 -0700

    Shapefile improvements

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ classifiers = [
 
 setup(
     name='rowgenerators',
-    version="0.8.11",
+    version="0.8.12",
     description='Generate row data from a variety of file formats',
     long_description=readme,
     packages=find_packages(),
