ddf888ecab8521a582afe29cc7f0109c589b151c
Eric Busboom <eric@civicknowledge.com>
2019-06-14 15:07:28 -0700

    Reved

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ classifiers = [
 
 setup(
     name='rowgenerators',
-    version="0.9.3",
+    version="0.9.4",
     description='Generate row data from a variety of file formats',
     long_description=readme,
     packages=find_packages(),
